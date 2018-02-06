7c471aa7999fafd22b4c2110434de0024163b68f
Andreas Gampe <agampe@google.com>
2018-02-06 13:38:03 -0800

    ART: Clear any exception after resolution
    When resolving a field to create a descriptive NullPointerException
    message, it is possible that resolution fails. In that case an
    exception will be pending, which needs to be cleared.
    
    Bug: 69729089
    Test: m test-art-host
    Change-Id: Ib69aac39e0d87c9400e1f4ce912dcc5dd703ecbc
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- runtime/common_throws.cc
+++ runtime/common_throws.cc
@@ -630,6 +630,7 @@ void ThrowNullPointerExceptionFromDexPC(bool check_address, uintptr_t addr) {
     case Instruction::IGET_SHORT: {
       ArtField* field =
           Runtime::Current()->GetClassLinker()->ResolveField(instr.VRegC_22c(), method, false);
+      Thread::Current()->ClearException();  // Resolution may fail, ignore.
       ThrowNullPointerExceptionForFieldAccess(field, true /* read */);
       break;
     }
@@ -662,6 +663,7 @@ void ThrowNullPointerExceptionFromDexPC(bool check_address, uintptr_t addr) {
     case Instruction::IPUT_SHORT: {
       ArtField* field =
           Runtime::Current()->GetClassLinker()->ResolveField(instr.VRegC_22c(), method, false);
+      Thread::Current()->ClearException();  // Resolution may fail, ignore.
       ThrowNullPointerExceptionForFieldAccess(field, false /* write */);
       break;
     }
