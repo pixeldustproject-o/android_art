hopez13/art

Generalize CompactDexDebugInfo offset table
Renamed the class to CompactOffsetTable.

Motivation: Use this table for quicken info offsets too.

Bug: 72608794
Bug: 63756964
Test: test-art-host-gtest
Change-Id: Idcb5355d8a5e4354bc3694152a7270047bed9d0a

dexlayout/compact_dex_writer.cc
@@ -19,7 +19,7 @@
 #include "android-base/stringprintf.h"
 #include "base/logging.h"
 #include "base/time_utils.h"
-#include "dex/compact_dex_debug_info.h"
+#include "dex/compact_offset_table.h"
 #include "dex/compact_dex_file.h"
 #include "dexlayout.h"
 
@@ -76,12 +76,12 @@ uint32_t CompactDexWriter::WriteDebugInfoOffsetTable(Stream* stream) {
   std::vector<uint8_t> data;
   debug_info_base_ = 0u;
   debug_info_offsets_table_offset_ = 0u;
-  CompactDexDebugInfoOffsetTable::Build(debug_info_offsets,
-                                        &data,
-                                        &debug_info_base_,
-                                        &debug_info_offsets_table_offset_);
+  CompactOffsetTable::Build(debug_info_offsets,
+                            &data,
+                            &debug_info_base_,
+                            &debug_info_offsets_table_offset_);
   // Align the table and write it out.
-  stream->AlignTo(CompactDexDebugInfoOffsetTable::kAlignment);
+  stream->AlignTo(CompactOffsetTable::kAlignment);
   debug_info_offsets_pos_ = stream->Tell();
   stream->Write(data.data(), data.size());
 
@@ -90,12 +90,12 @@ uint32_t CompactDexWriter::WriteDebugInfoOffsetTable(Stream* stream) {
   if (kMeasureAndTestOutput && !debug_info_offsets.empty()) {
     uint64_t start_time = NanoTime();
     stream->Begin();
-    CompactDexDebugInfoOffsetTable::Accessor accessor(stream->Begin() + debug_info_offsets_pos_,
-                                                      debug_info_base_,
-                                                      debug_info_offsets_table_offset_);
+    CompactOffsetTable::Accessor accessor(stream->Begin() + debug_info_offsets_pos_,
+                                          debug_info_base_,
+                                          debug_info_offsets_table_offset_);
 
     for (size_t i = 0; i < debug_info_offsets.size(); ++i) {
-      CHECK_EQ(accessor.GetDebugInfoOffset(i), debug_info_offsets[i]);
+      CHECK_EQ(accessor.GetOffset(i), debug_info_offsets[i]);
     }
     uint64_t end_time = NanoTime();
     VLOG(dex) << "Average lookup time (ns) for debug info offsets: "

pixeldustproject-o/android_art

Signed-off-by: mydongistiny <jaysonedson@gmail.com>

dexlayout/compact_dex_writer.cc
@@ -18,8 +18,8 @@
 
 #include "base/logging.h"
 #include "base/time_utils.h"
-#include "dex/compact_dex_debug_info.h"
 #include "dex/compact_dex_file.h"
+#include "dex/compact_offset_table.h"
 #include "dexlayout.h"
 
 namespace art {
@@ -74,12 +74,12 @@ uint32_t CompactDexWriter::WriteDebugInfoOffsetTable(uint32_t offset) {
   std::vector<uint8_t> data;
   debug_info_base_ = 0u;
   debug_info_offsets_table_offset_ = 0u;
-  CompactDexDebugInfoOffsetTable::Build(debug_info_offsets,
-                                        &data,
-                                        &debug_info_base_,
-                                        &debug_info_offsets_table_offset_);
+  CompactOffsetTable::Build(debug_info_offsets,
+                            &data,
+                            &debug_info_base_,
+                            &debug_info_offsets_table_offset_);
   // Align the table and write it out.
-  offset = RoundUp(offset, CompactDexDebugInfoOffsetTable::kAlignment);
+  offset = RoundUp(offset, CompactOffsetTable::kAlignment);
   debug_info_offsets_pos_ = offset;
   offset += Write(data.data(), data.size(), offset);
 
@@ -87,12 +87,12 @@ uint32_t CompactDexWriter::WriteDebugInfoOffsetTable(uint32_t offset) {
   const bool kMeasureAndTestOutput = dex_layout_->GetOptions().verify_output_;
   if (kMeasureAndTestOutput && !debug_info_offsets.empty()) {
     uint64_t start_time = NanoTime();
-    CompactDexDebugInfoOffsetTable::Accessor accessor(mem_map_->Begin() + debug_info_offsets_pos_,
-                                                      debug_info_base_,
-                                                      debug_info_offsets_table_offset_);
+    CompactOffsetTable::Accessor accessor(mem_map_->Begin() + debug_info_offsets_pos_,
+                                          debug_info_base_,
+                                          debug_info_offsets_table_offset_);
 
     for (size_t i = 0; i < debug_info_offsets.size(); ++i) {
-      CHECK_EQ(accessor.GetDebugInfoOffset(i), debug_info_offsets[i]);
+      CHECK_EQ(accessor.GetOffset(i), debug_info_offsets[i]);
     }
     uint64_t end_time = NanoTime();
     VLOG(dex) << "Average lookup time (ns) for debug info offsets: "